987e921aeb0b28086b4005bbbdfcca02d89ac329
bhandychoi <bzc164@psu.edu>
2019-04-23 14:54:17 -0400

    Files organized

--- cali_b.m
+++
@@ -1,9 +0,0 @@
-function X=cali_b(delta_L,y_L,r,tar_wL,k,A,b)
-x=tar_wL*y_L;
-the_L=(1+r)*(y_L-x)/((r+delta_L)*k)-A;
-p=the_L/(A+the_L);
-
-dp=-A/(A+the_L)^2*(1+r)/((r+delta_L)*k);
-X= ( (1+r)*(dp*x+p)-(r+delta_L)*dp*b*y_L )*(r+delta_L+(1-delta_L)*p)...
-    -(1-delta_L)*dp*( (1+r)*p*x+(r+delta_L)*(1-p)*b*y_L  );
-end
--- main.m
+++ main.m
@@ -10,4 +10,4 @@ delta_L=0.04;
 load('para.mat')
 delta_H=0.0157;
 
-[u_H,u_L,w_L,w_H,p_L,p_H,wage_ratio, wage_output_ratio_L]=sole_steady(y_L,y_H,k,r,delta_L,A,b,delta_H)
+[u_H,u_L,w_L,w_H,p_L,p_H,wage_ratio, wage_output_ratio_L] = sole_steady(y_L,y_H,k,r,delta_L,A,b,delta_H)
