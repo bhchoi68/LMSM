280118cf7eeb1d62360b7d64f85c1a1cb1bde0ab
bhandychoi <bzc164@psu.edu>
2019-04-29 14:40:10 -0400

    Codes changed

--- main.m
+++ main.m
@@ -1,13 +1,12 @@
-clc;
-clear;
+clc; clear;
 
-y_L=1;
-y_H=2;
-k=0.25;
-r=0.004;
-delta_L=0.04;
+y_L     = 1;
+y_H     = 2;
+k       = 0.25;
+r       = 0.004;
+delta_L = 0.04;
+delta_H = 0.0157;
 
-load('para.mat')
-delta_H=0.0157;
+load('para.mat');
 
 [u_H,u_L,w_L,w_H,p_L,p_H,wage_ratio, wage_output_ratio_L] = sole_steady(y_L,y_H,k,r,delta_L,A,b,delta_H)
